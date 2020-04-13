90fbd5e4d74503780d1e030862a4fd860d766c3c
Ashleytho <60194041+Ashleytho@users.noreply.github.com>
2020-04-13 19:15:34 -0400

    test api keys

---
+++ tweet_api_getter.py
@@ -0,0 +1,10 @@
+import tweepy
+
+auth = tweepy.OAuthHandler('wovXmgwNTBOaGiSvHHxFWA8c7', 'tRttLbA13KzUyTlFk96wwX9ACqzuI0RgqHYCBVcECgcSGB44Hw')
+auth.set_access_token('3299016720-cgw3M5OKYbf8BzUHgaVBdVctynygwLTYzngbLsT', '2SjWmJS6VHCd4ITPIKBWBXJdGBVG74ys2OhGoKoN2Pika')
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)
