d63e1b26ff531cf890002fa942586053ca9f2df8
Alex Jones <acjreno@umich.edu>
2020-04-15 11:36:43 -0700

    Housekeeping and general setup.

---
+++ main.py
@@ -0,0 +1,15 @@
+## Alex Jones, Ashley Thompson
+## SI 206 - Data Oriented Programming
+## University of Michigan
+##
+## The main function for our data collection project.
+
+from utility import set_up_main_db
+
+def __main__():
+    #print("Welcome to Elon's Expensive Stocks, a data collection project by Ashley Thompson and Alex ")
+    set_up_main_db()
+
+
+if __name__ == '__main__':
+    __main__()
